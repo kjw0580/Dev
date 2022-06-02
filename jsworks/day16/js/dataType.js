29b050e5b5ecfda12a2f59e7b8c9a913337a3bb6
kjw0580 <kjw0580@naver.com>
2022-06-02 18:15:41 +0900

    dddd

--- jsworks/day16/js/dataType.js
+++ jsworks/day16/js/dataType.js
@@ -1,46 +1,40 @@
-window.onload = function(){
 //number
-var num1 = 10;
-var num2 = 10.2;
-var num3 = 1e+2; //지수(e) 10의 세제곱 
+window.onload = function(){ //실행함수-이름없는 함수
+    let num1 = 10;
+    let num2 = 10.2
+    let num3 = 1e+3;  //지수(e) 10의 세제곱
 
-console.log(num1 + ", " + num2 + ", " + num3);
+    console.log(num1 + "," + num2 + "," + num3);
 
-//string
+    //string
+    let str1 = "문자";
+    let str2 = "문자는 '문자'";
+    let str3 = '문자는 "문자"';
+    //let str4 = "you're too smart...";
+    let str4 = 'you\'re too smart...';
+    console.log(str1 + "," + str2 + "," + 
+                str3 + "," + str4);  
 
-var str1 = '문자';
-var str2 = "문자는 '문자'" ;
-var str3 = '문자는 "문자"';
-var str4 = 'you\'re too smart...';
-console.log(str1 + ", " + str2 + ", " + str3 + ", " + str4);
+    //boolean
+    let com1 = ( 5 > 4);
+    let com2 = ( 5 < 4);
 
+    console.log(com1 + ", " + com2);
 
+    //undefined
+    let storage; //변수 선언 후 값을 입력하지 않음
+    //storage = "A";
+    console.log(storage);
 
-//boolean
-let com1 = (5 > 4);
-let com2 = (5 < 4);
+    //null
+    let gnb = document.getElementById('gnb');
+    // gnb.style.listStyleType = "none";
+    console.log(gnb);
 
-console.log(com1 + ", " + com2)
-
-//undefined
-let storage; //변수 선언 후 값을 입력 하지 않음
-//storage = "A";
-console.log(storage);
-
-
-
-//null
-let gnb = document.getElementById('gnb');
-gnb.style.listStyle = "none";
-console.log(gnb);
-
-let qwe = document.getElementById('demo');
-qwe.innerHTML = "안녕하세요";
-console.log(qwe);
-
-//type of
-let type1 = 10;
-let type2 = '문자';
-let type3 = true;
-console.log(typeof(type1) + ", " + typeof(type2) + ", " + typeof type3)
+    //type of
+    let type1 = 10;
+    let type2 = '문자';
+    let type3 = true;
+    console.log(typeof(type1) + "," + typeof type2 
+                + "," + typeof type3);
 }
